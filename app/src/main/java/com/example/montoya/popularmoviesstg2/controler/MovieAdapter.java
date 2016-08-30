aaf30a00ff641f1612297f2e14d6a69af7e48fec
montoya <gabi.montoya@gmail.com>
2016-08-30 11:05:53 +0200

    2.0.105 - It was created the fetch movie task which get the data from the internet and stores it into the database.

---
+++ app/src/main/java/com/example/montoya/popularmoviesstg2/controler/MovieAdapter.java
@@ -0,0 +1,26 @@
+package com.example.montoya.popularmoviesstg2.controler;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+
+/**
+ * Created by montoya on 30.08.2016.
+ */
+public class MovieAdapter extends CursorAdapter{
+    public MovieAdapter(Context context, Cursor c, int flags) {
+        super(context, c, flags);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
+        return null;
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+    }
+}
